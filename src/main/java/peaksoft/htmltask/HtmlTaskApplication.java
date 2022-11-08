b6b5656257750e80c01035010a0c9026afcf748e
ulukbekkochkorov <Ulukbekkochkorov@gmail.com>
2022-11-08 22:26:49 +0600

    Initial commit

---
+++ src/main/java/peaksoft/htmltask/HtmlTaskApplication.java
@@ -0,0 +1,13 @@
+package peaksoft.htmltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/peaksoft/htmltask/HtmlTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.htmltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
